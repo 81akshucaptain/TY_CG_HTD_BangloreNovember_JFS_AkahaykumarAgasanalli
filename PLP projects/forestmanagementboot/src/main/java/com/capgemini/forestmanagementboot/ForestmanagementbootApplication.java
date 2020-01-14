fd2d8c4ab3abe1b242c63579ebbb339a5279be70
Akshaykumar Agasanalli <aagasanalli@gmail.com>
2020-01-14 20:23:03 +0530

    FMSboot

---
+++ PLP projects/forestmanagementboot/src/main/java/com/capgemini/forestmanagementboot/ForestmanagementbootApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.forestmanagementboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestmanagementbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestmanagementbootApplication.class, args);
+	}
+
+}
---
+++ PLP projects/forestmanagementboot/src/test/java/com/capgemini/forestmanagementboot/ForestmanagementbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.forestmanagementboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestmanagementbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
