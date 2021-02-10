0b8e9ecfba6ed584b174891f4fc2cb29008fa218
Akshaykumar Agasanalli <aagasanalli@gmail.com>
2021-02-10 11:01:46 +0530

    PRACTICE

---
+++ Akshay-BACK end codes/spring PRACTICE/spring-demo-core/src/com/tcs/springdemo/SadFortuneService.java
@@ -0,0 +1,11 @@
+package com.tcs.springdemo;
+
+public class SadFortuneService implements FortuneService {
+
+	@Override
+	public String getMyFortuneToday() {
+		// TODO Auto-generated method stub
+		return "Sad day for US";
+	}
+
+}
